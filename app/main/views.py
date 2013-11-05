3ffe464cc4bd16fa60d3a8ab0b2792c47f142985
Klead3r <asd@yolo.fake.com>
2013-11-05 13:45:49 +0200

    working on user authentication

--- app/main/views.py
+++ app/main/views.py
@@ -4,13 +4,17 @@ from flask import Blueprint, request, render_template, flash,\
 from flask.ext.login import login_user, logout_user,\
     current_user, login_required
 
+from app.main.forms import LoginForm, RegistrationForm
+
 #from app.main.forms import RegisterForm, LoginForm,\
 #    UserForm
 
-from werkzeug import check_password_hash, generate_password_hash
+#from werkzeug import check_password_hash, generate_password_hash
 
-from app import app, db
+from app import db, bcrypt
 from app.main.models import User
+
+#from flask.ext.admin import helpers
 
 
 main = Blueprint('main', __name__, template_folder='../templates/main/')
@@ -20,9 +24,11 @@ main = Blueprint('main', __name__, template_folder='../templates/main/')
 def index():
     return 'hello2'
 
+
 @main.route('/hi')
 def hi():
     return 'hi'
+
 
 @main.route('/login/', methods=['GET', 'POST'])
 def login():
@@ -44,6 +50,24 @@ def login():
             return redirect(request.args.get('next') or url_for('main.home'))
         flash('Wrong email or password', 'error-message')
     return render_template("user/login.html", form=form)
+
+
+@main.route('/register/', methods=['GET', 'POST'])
+def register_view():
+    form = RegistrationForm(request.form)
+    if form.validate_form_on_submit(form):
+
+        user = User(name=form.name.data, email=form.email.data,
+                    password=bcrypt.generate_password_hash(form.password.data))
+
+        # Insert the record in our database and commit it
+        db.session.add(user)
+        db.session.commit()
+
+        login.login_user(user)
+        return redirect(url_for('index'))
+
+    return render_template('form.html', form=form)
 
 
 @main.route("/logout/")
